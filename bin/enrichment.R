2e1ac3d9207ebda9f89ff14574a77d7379057193
Francesco L <francesco.lescai@unipv.it>
2025-06-13 11:01:08 +0000

    fixed missing shebang

--- bin/enrichment.R
+++ bin/enrichment.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 #### Load the library ####
 library(tidyverse)
 library(clusterProfiler)
--- bin/subset_gff.R
+++ bin/subset_gff.R
@@ -1,4 +1,4 @@
-#!/usr/bin/env R
+#!/usr/bin/env Rscript
 
 #### Load the libraries ####
 library(rtracklayer)
